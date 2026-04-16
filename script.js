fdc8d7f5e80a5fd248b06f8b05c3deaafe1f477a
jillielloyd-wq <jillielloyd@hunschool.org>
2026-04-16 18:14:12 -0400

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 window.onload = function() {
-    
-    // This finds your button
     const btn = document.getElementById("factButton");
 
-    // This makes the pop-up happen when clicked
     btn.onclick = function() {
         alert("🌸 The cherry blossom is the national flower of Japan! 🌸");
+        createPetals();
     };
-
 };
+
+function createPetals() {
+    const petalColors = ["🌸", "💮", "💖"]; 
+    
+    for (let i = 0; i < 50; i++) {
+        setTimeout(() => {
+            const petal = document.createElement("div");
+            petal.className = "petal";
+            petal.innerHTML = petalColors[Math.floor(Math.random() * petalColors.length)];
+            petal.style.left = Math.random() * 100 + "vw";
+            petal.style.animationDuration = (Math.random() * 2 + 3) + "s";
+            document.body.appendChild(petal);
+        }, i * 100); 
+    }
+}
